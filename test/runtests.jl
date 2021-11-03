482c4a792d9da8db902584d347bbf1f5b5d32355
Zelun Wang <zelunwang@gmail.com>
2021-11-03 00:44:22 -0500

    graph #1 test fail

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,19 @@ using Test
 
 @testset "TDD.jl" begin
     # Write your tests here.
+  
+    #1.
+    @test returnDCneighbors(1) == [1, 2, 3];
+    @test returnDCneighbors(2) == [2, 1];
+    @test returnDCneighbors(3) == [3, 1];
+    @test returnDCneighbors(4) == [4, 5];
+    @test returnDCneighbors(5) == [5, 4];
+
+
 end
+
+
+
+
+
+
