afacf9eab74b9d3ed1a6c3c95933f3ca5b5fb031
Zelun Wang <zelunwang@gmail.com>
2021-11-09 14:58:31 -0600

    graph #1 implementation

--- src/TDD.jl
+++ src/TDD.jl
@@ -2,11 +2,40 @@ module TDD
 
 # Write your package code here.
 
+#1
 
+graph = [ 
+    [2, 3], #nodes reacheable from node 1
+    [1],    #nodes reacheable from node 2
+    [1],    #nodes reacheable from node 3
+    [5],    #nodes reacheable from node 4
+    [4]     #nodes reacheable from node 5
+]
 
-function returnDCneighbors(node::Int64)
+function returnDCneighbors(graph_input, node::Int64) #function argument type declaration for graph_input works like this? Since it is an object of Array{Array{Int64,1},1}
+
+    try        
+     vcat(node, graph_input[node])
+    catch e
+        if isa(e, BoundsError)
+            error("node does not exist")
+        else
+         
+        end
+    end
         
 end
+    
+     
+    
+
+
+
+
+
+
+
+
 
 
 end
