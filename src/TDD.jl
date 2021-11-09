489e946c896fae69daa866279308e383330c4f1a
Zelun Wang <zelunwang@gmail.com>
2021-11-09 17:34:41 -0600

    Change DCneighbors to return a Set

--- src/TDD.jl
+++ src/TDD.jl
@@ -9,7 +9,7 @@ export DCneighbors
 function DCneighbors(graph_input, node::Int64) #function argument type declaration for graph_input works like this? Since it is an object of Array{Array{Int64,1},1}
 
     try        
-     vcat(node, graph_input[node])
+     Set(vcat(node, graph_input[node])) #Sets are unordered
     catch e
         if isa(e, BoundsError)
             error("node does not exist")
