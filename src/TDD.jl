f725c804006db8ba0ffa4e997a9af9b63964f557
Zelun Wang <zelunwang@gmail.com>
2021-11-09 16:48:13 -0600

    tweak updates
    moved graph declaration to test
    added directed graph
    changed #1 function name to DCneighbors

--- src/TDD.jl
+++ src/TDD.jl
@@ -4,15 +4,9 @@ module TDD
 
 #1
 
-graph = [ 
-    [2, 3], #nodes reacheable from node 1
-    [1],    #nodes reacheable from node 2
-    [1],    #nodes reacheable from node 3
-    [5],    #nodes reacheable from node 4
-    [4]     #nodes reacheable from node 5
-]
 
-function returnDCneighbors(graph_input, node::Int64) #function argument type declaration for graph_input works like this? Since it is an object of Array{Array{Int64,1},1}
+
+function DCneighbors(graph_input, node::Int64) #function argument type declaration for graph_input works like this? Since it is an object of Array{Array{Int64,1},1}
 
     try        
      vcat(node, graph_input[node])
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,46 @@ using Test
     # Write your tests here.
   
     #1.
-    @test returnDCneighbors(graph, 1) == [1, 2, 3];
-    @test returnDCneighbors(graph, 2) == [2, 1];
-    @test returnDCneighbors(graph, 3) == [3, 1];
-    @test returnDCneighbors(graph, 4) == [4, 5];
-    @test returnDCneighbors(graph, 5) == [5, 4];
-    @test_throws ErrorException("node does not exist") returnDCneighbors(graph, 6);
+
+    
+    undirected_graph = [ 
+        [2, 3], #nodes reacheable from node 1
+        [1],    #nodes reacheable from node 2
+        [1],    #nodes reacheable from node 3
+        [5],    #nodes reacheable from node 4
+        [4]     #nodes reacheable from node 5
+    ]
+
+    #based on Figure 1 from homework, node order 2 --> 1 --> 3; 4 --> 5
+    directed_graph = [ 
+        [3],    #nodes reacheable from node 1
+        [1],    #nodes reacheable from node 2
+        [],     #nodes reacheable from node 3
+        [5],    #nodes reacheable from node 4
+        []      #nodes reacheable from node 5
+    ]
+
+
+
+    @test DCneighbors(undirected_graph, 1) == [1, 2, 3];
+    @test DCneighbors(undirected_graph, 2) == [2, 1];
+    @test DCneighbors(undirected_graph, 3) == [3, 1];
+    @test DCneighbors(undirected_graph, 4) == [4, 5];
+    @test DCneighbors(undirected_graph, 5) == [5, 4];
+
+    @test DCneighbors(directed_graph, 1) == [1, 3];
+    @test DCneighbors(directed_graph, 2) == [2, 1];
+    @test DCneighbors(directed_graph, 3) == [3];
+    @test DCneighbors(directed_graph, 4) == [4, 5];
+    @test DCneighbors(directed_graph, 5) == [5];
+    
+    @test_throws ErrorException("node does not exist") DCneighbors(undirected_graph, 6);
+
+
+
+    #2
+
+
 
 
 end
