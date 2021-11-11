ca6decdd010173d7e19fc51f1a5701f04de6483e
Zelun Wang <zelunwang@gmail.com>
2021-11-10 23:59:29 -0600

    Passes all tests for graph problem #2
    switched to return ConnectedToNode_Array[z]
    Cleaned comments

--- src/TDD.jl
+++ src/TDD.jl
@@ -44,7 +44,7 @@ function ICneighbors(graph_input, node::Int64) #this is way convoluted implement
     ConnectedToNode = [] 
     for i in 1:length(graph_input) 
         if in(node, AllNodes[i]) == true
-            union!(ConnectedToNode, AllNodes[i]) #ConnectedToNode is now an array (that has been unioned)...I think that's cuz ConnectedToNode is an array...
+            union!(ConnectedToNode, AllNodes[i]) #ConnectedToNode is now an array cuz ConnectedToNode is an array
                                                  #note: a more logical implementation would be to just find all i that have connection to 'node'. But this works...
                                                  #wondering which way would perform faster...                           
         else
@@ -55,12 +55,7 @@ function ICneighbors(graph_input, node::Int64) #this is way convoluted implement
     push!(ConnectedToNode_Array, Set([node])) #this starts ConnectedToNode_Array[1] so that ConnectedToNode pushes to ConnectedToNode_Array[2], so that
                                               #nodeset_input[z-1] does not eval nodeset_input[0]
                                               #note that this becomes a Set because pushing a Set into an empty array Any[]
-    
     push!(ConnectedToNode_Array, Set( convert(Array{Int64,1}, ConnectedToNode) ) ) #https://stackoverflow.com/questions/35482527/how-do-i-change-the-data-type-of-a-julia-array-from-any-to-float64
-
-    
-
-    #honestly I think up to this point it works as intended
 
 
     #now can start recursive function
@@ -68,43 +63,26 @@ function ICneighbors(graph_input, node::Int64) #this is way convoluted implement
     z_input = 2
 
     function recursor(graph_input_input, AllNodes_input, nodeset_input, z) #the reason I'm doing this again is I don't know how to get around reinitializing
-                                                                        #the ConnectedToNode object with every run
-                                                                        #so this way ConnectedToNode_Array is initialized outside the function...
-                                                                        #also when I highlight this function to evaluate in REPL it seems to evaluate like 5 times...why? Seen this with other scripts too
+                                                                         #the ConnectedToNode object with every run
+                                                                         #so this way ConnectedToNode_Array is initialized outside the function...
+                                                                         #also when I highlight this function to evaluate in REPL it seems to evaluate like 5 times...why? Seen this with other scripts too
 
-        if ( nodeset_input[z] == nodeset_input[z - 1] )  &  ( length(nodeset_input) == (2 + length(graph_input_input)) )
-
-                                                    #this could produce incorrect exit with terminal nodes with 1 partner since union returns
-                                                    #the nodeset_input[~next~] as something == the previous
-                                                    #need to force it to iterate through the latest nodeset_input one more time
-                                                    #change nodeset_input[z] == nodeset_input[z - 1] to nodeset_input[z] == nodeset_input[z - 2]
-                                                        #^reverted
-                                                    #z_input = 2 to z_input = 3
-                                                        #^reverted
-                                                    #added another push step in initialization of ConnectedToNode_Array
-                                                        #^reverted
-                                                    #hmm that doesnt help, just prolonged the problem
-                                                    #no also this occurs with non terminal cases too. Makes me think my recursion is not working right
-                                                        #fixed, see below the in.()
-                                                    #now works for most cases except node 4,5,6,7,8 of train_graph
-                                                    #problem lies in recursor always restarting with node
-                                                    #for example if node 8, then tracing the ConnectedToNode_Array stack,
-                                                    #the stack of nodeset_input[z] would be [8], [8, 7, 6], [8, 7, 6] which would trigger early exit
-                                                    #add an & statement to if exit condition to fix
-
-            return ConnectedToNode_Array #return ConnectedToNode_Array to see stack of all connected nodes
+        if ( nodeset_input[z] == nodeset_input[z - 1] )  &  ( length(nodeset_input) == (2 + length(graph_input_input)) )                                                
+                                                    #earlier had trouble with the in() test always returning false so recursor was missing union! steps. Now fixed, see below in.()
+                                                    #At this point it worked for most cases except node 4,5,6,7,8 of train_graph
+                                                    #Fixed by adding an & statement to if exit condition to fix problem with recursor starting again at node and triggering early exit. Force more repetitions bypasses early exit
+            return ConnectedToNode_Array[z] #return ConnectedToNode_Array to see stack of all connected nodes for debugging
 
         else
 
             for i in 1:length(graph_input_input) 
-                if any( in.(nodeset_input[z], Ref(AllNodes_input[i])) ) == true #THIS IS NOT WORKING HOW I THINK IT WORKS #fixed. See docs for in() and any()
+                if any( in.(nodeset_input[z], Ref(AllNodes_input[i])) ) == true #See docs for in() and any()
                     union!(nodeset_input[z], AllNodes_input[i]) #Set union with Set gives Set
                 else
                 end
             end
 
-            push!(nodeset_input, nodeset_input[z]) #this already gives a Set since both those are Sets
-            
+            push!(nodeset_input, nodeset_input[z]) #this already gives a Set since both those are Set
             recursor(graph_input_input, AllNodes_input, nodeset_input, z+1)
             
         end
@@ -112,11 +90,6 @@ function ICneighbors(graph_input, node::Int64) #this is way convoluted implement
 
     #call
     recursor(graph_input, AllNodes, ConnectedToNode_Array, z_input)
-
-
-
-
-   
 
 end #function ICneighbors end
 
